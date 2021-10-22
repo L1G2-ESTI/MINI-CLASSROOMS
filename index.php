077dea7a176b6e8ded1e83c03559be1584b198f5
SergioDev22 <louis.sergio.rakotoarisoa@esti.mg>
2021-10-22 10:52:49 +0300

    Le fichier routeur

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Home Mini-Classroom</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="assets/css/index.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col col-md-6 col-md-offset-3">
+				<div style="background-color: red;">accueil</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
