51e414c7d98e96552803f19f37292301c5bf32f8
SergioDev22 <louis.sergio.rakotoarisoa@esti.mg>
2021-10-25 14:24:15 +0300

    le fichier MOdel

--- Model/Model.php
+++ Model/Model.php
@@ -1,1 +1,19 @@
- 
+<?php
+    class connect_bdd {
+        protected function dbconnect() {
+            try{
+                $connexion = new PDO('mysql:host=127.0.0.1;dbname=','', '');
+                return $connexion;
+            }catch(PDOException $erreurs){
+                die($erreurs->getMessage());
+            };
+        }
+    }
+
+    class query_data extends connect_bdd {
+
+        // tous les methodes des requetes reliés à la BDD
+
+    }
+
+?>
