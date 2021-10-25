5dbfef76843ebb8050828af05dc02153a9f78eb9
SergioDev22 <louis.sergio.rakotoarisoa@esti.mg>
2021-10-25 14:01:41 +0300

    le fichier controller

--- Controllers/controller.php
+++ Controllers/controller.php
@@ -1,1 +1,11 @@
- 
+<?php
+    require_once("Model/Model.php");
+
+    function acceuil(){
+        try{
+            require("Views/login.html");
+        }catch (Exception $e){
+            die($e->getMessage());
+        }
+    }
+?>
