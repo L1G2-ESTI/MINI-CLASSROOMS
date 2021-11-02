b219d61333358c1c13f1fda2ecec423640c667dc
SergioDev22 <louis.sergio.rakotoarisoa@esti.mg>
2021-11-03 01:09:58 +0300

    sergio

--- Views/acceuilEtudiant.php
+++ Views/acceuilEtudiant.php
@@ -25,15 +25,19 @@
 
 	<section>
 		<div class="baniere"></div>
-		<div>
-			<img class="profil-prof" src="assets/img/prof.jpeg" width="150px" height="150px">
-		</div>
+		<?php
+			echo '
+			<div>
+			<img src="'.$user_nom['photo'].'" class="profil-prof" width="150px" height="150px">
+			</div>
+			';
+		?>
 	</section>
 
 	<section>
 		<div class="container-prof">
 			<div class="prof-information">
-				<h3></h3>
+				<h3><?=$user_nom["nom_etud"]." ".$user_nom["pre_etud"]?></h3>
 				<div class="reseaux">
 					<img src="assets/img/logo/g+.png" width="20px" height="20px">
 					<li><a href="#"><?=$_POST["email"]?></a></li>
@@ -45,23 +49,30 @@
 				<!-- <div class="reseaux">
 					<img src="assets/img/logo/4sq.png" width="20px" height="20px">
 					<li>Amboditsiry, Madagascar</li>
-				</div> -->
+				</div>-->
 				<input type="submit" name="envoie" value="Envoyer Mail">
 			</div>
-			<div class="prof-module">
-				<div class="card-module">
-					<div class="fond-haut">
-						<h1>Francais</h1>
-					</div>
-					<div class="logo-module">
-						<p>110</p>
-					</div>
-					<div class="corps-module">
-						<input type="button" name="enter" value="Ouvrir">
-					</div>
+				<div class="prof-module">
+					<?php 
+						while ($user_cours=$cours->fetch()){
+							echo '
+									<div class="card-module">
+										<div class="fond-haut">
+											<h1>'.$user_cours["nom_mat"].'</h1>
+										</div>
+										<div class="logo-module">
+											<p>'.$user_cours["id_module"].'</p>
+
+										</div>
+										<div class="corps-module">
+											<input type="button" onclick=window.location.href="https://www.facebook.com"; name="enter" value="Ouvrir">
+										</div>
+									</div>
+								';
+						}
+					?>
 				</div>
-				
-			</div>
+
 		</div>
 	</section>
 
